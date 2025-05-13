fef7cc89e2e437bbafe0fc1ad44e3db38674e2f7
Kurabachew <kmengistu1216@gmail.com>
2025-05-13 17:07:45 -0500

    banner

--- netflix-clone/src/utils/axios.jsx
+++ netflix-clone/src/utils/axios.jsx
@@ -1,11 +1,4 @@
-// import axios from 'axios'
-// const instance = axios.create({
-//      baseURL: "https://api.themoviedb.org/3",
-// });
 
-// export default instance;
-
-// utils/axios.js
 import axios from 'axios';
 
 const instance = axios.create({
